30ce03a25bce1f0d279e27ca13b94e00f2fa6c5e
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-03-16 13:33:10 +0300

    API class init

---
+++ src/js/Api.js
@@ -0,0 +1,24 @@
+export default class API {
+  constructor(url) {
+    this.url = url;
+    this.contentTypeHeader = { 'Content-Type': 'application/json' };
+  }
+
+  load() {
+    return fetch(this.url);
+  }
+
+  add(user) {
+    return fetch(this.url, {
+      body: JSON.stringify(user),
+      method: 'POST',
+      headers: this.contentTypeHeader,
+    });
+  }
+
+  remove(id) {
+    return fetch(`${this.url}/${id}`, {
+      method: 'DELETE',
+    });
+  }
+}
